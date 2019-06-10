2787a7c8dca37b418a00dcd3d8edfeac3e8f132a
Emiel Steerneman <emielsteerneman@gmail.com>
2019-06-10 17:54:23 +0200

    more work on visualisation

--- webcam.py
+++ webcam.py
@@ -8,7 +8,7 @@ from PIL import Image
 import numpy as np
 import time
 import sys
-
+from torchsummary import summary
 # Numpy pretty print
 np.set_printoptions(precision=2)
 
@@ -24,6 +24,7 @@ else:
 
 # Set model to evaluation mode (disables dropout layers)
 model.eval()
+summary(model, (1, IMAGE_SIZE, IMAGE_SIZE), 1)
 
 ### Load class labels
 predictions = {}
@@ -42,24 +43,46 @@ HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # writer = cv2.VideoWriter("/home/emiel/Desktop/REDI_.mp4", cv2.VideoWriter_fourcc(*'XVID'), 30, (WIDTH, HEIGHT))
 
-print("Adding hooks to model..")
-layerOutputs = [None] * len(model.convLayers) 
+BINARY_THRESHOLD = 130
+NP_THRESHOLD = 0.3
+
+
+
+##########################
+### ADD HOOKS TO MODEL ###
+convActivations  = np.zeros((len(model.convLayers), NCHANNELS))
+convActivationsT = np.zeros((len(model.convLayers), NCHANNELS))
+fullActivations  = np.zeros((1, NCHANNELS * 4 * 4))
+fullActivationsT = np.zeros((1, NCHANNELS * 4 * 4))
+# fullActivations  = np.zeros(NCHANNELS * 4 * 4)
+# fullActivationsT = np.zeros(NCHANNELS * 4 * 4)
 
 def hook_fn(iLayer, module, input, output):
 	# Get data
 	activations = output.data[0].numpy().copy()
 	# Get average of activations
 	activations = activations.max(axis=(1, 2))
-	activations -= np.min(activations)
-	activations /= np.max(activations)
-	layerOutputs[iLayer] = activations
-	print(iLayer, "triggered", activations)
+	convActivations[iLayer] = activations.copy()
+	convActivationsT[iLayer] += 0.1 * (convActivations[iLayer] - convActivationsT[iLayer])
 
+def dropout_hook_fn(module, input, output):
+	fullActivations = input[0].data[0].numpy()
+	fullActivationsT[0] += 0.1 * (fullActivations - fullActivationsT[0])
 
-### Add hooks to model
+print("Adding hooks to model..")
 for iLayer, layer in enumerate(model.convLayers):
 	# Ugly hack to create a closure for the variable iLayer. https://stackoverflow.com/a/2295368/2165613
-	layer.register_forward_hook(	(lambda iL : lambda module, input, output : hook_fn(iL, module, input, output))(iLayer)		)
+	layer.register_forward_hook(	  (lambda iL : lambda module, input, output : hook_fn(iL, module, input, output))(iLayer)		)
+model.drop_out.register_forward_hook(dropout_hook_fn)
+### HOOKS ADDED TO MODEL ### 
+############################
+
+###################################
+### EVERYTHING BARCHART RELATED ###
+COLOURS = [(255, 127, 0), (127, 0, 255), (0, 255, 127), (127, 255, 0), (255, 0, 127), (0, 127, 255)]
+def getColour(i):
+	return COLOURS[ i % len(COLOURS) ]
+
 
 print("Loading single image..")
 airplane = dataloader.loadAndPrepImage("sketches_png/apple/322.png", imsize=IMAGE_SIZE).reshape(IMAGE_SIZE, IMAGE_SIZE)
@@ -76,9 +99,11 @@ def classifyImage(model, image):
 	im = 1 - im 				# Invert image. Black drawing becomes white
 
 	## Convert image to binary
-	indices = im < 0.2			# Threshold image
+	indices = im < NP_THRESHOLD # Threshold image
 	im[indices] = 0				# Float to binary
 	im[~indices]= 1				# Float to binary
+
+	# cv2.imshow("thresh", im)
 
 	## Convert to FloatTensor and put through network
 	_input = torch.FloatTensor( im.reshape(1, 1, IMAGE_SIZE, IMAGE_SIZE) )
@@ -111,7 +136,7 @@ while(True):
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	blur = cv2.GaussianBlur(gray, (5, 5), 0)
-	_, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+	_, binary = cv2.threshold(gray, BINARY_THRESHOLD, 255, cv2.THRESH_BINARY_INV)
 
 	### Find all squares in the image
 	contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -128,7 +153,7 @@ while(True):
 		x1, x2 = x + w//10, x + w - w//10
 		y1, y2 = y + h//10, y + h - h//10
 		squaresFound.append([x1, y1, x2, y2, x, y, h, w])
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+		# cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
 	# cv2.imshow("binary", binary)
 
@@ -142,7 +167,7 @@ while(True):
 
 	# Just grab the last square for now
 	x1, y1, x2, y2, x, y, h, w = squaresFound[-1]
-	cv2.rectangle(frame,(x,y),(x+w,y+h),(255, 0, 0),2)		
+	cv2.rectangle(frame,(x,y),(x+w,y+h),(255, 128, 0),2)		
 	drawing = gray[y1:y2, x1:x2]
 
 	# Classify image
@@ -172,17 +197,49 @@ while(True):
 	frame[y0:y1, x0:x1, 1] = im
 	frame[y0:y1, x0:x1, 2] = im
 
-
-	# Visualize activations
-	BAR_WIDTH = 22
-	for iLayer, activations in enumerate(layerOutputs): 
+	###########################
+	### Visualize bar chart ###
+	BAR_WIDTH, PADDING = 20, 2
+	LAYER_WIDTH = NCHANNELS * (BAR_WIDTH+PADDING) - PADDING + BAR_WIDTH
+	GRAPH_WIDTH = BAR_WIDTH + NLAYERS * LAYER_WIDTH
+	
+	chart = np.ones((400, GRAPH_WIDTH, 3), dtype=np.uint8) * 20
+	for iLayer, activations in enumerate(convActivationsT):
+		x = BAR_WIDTH + iLayer * LAYER_WIDTH
 		for iA, a in enumerate(activations):
-			x = WIDTH - BAR_WIDTH * (iA+2)
-			y = HEIGHT - 10 - iLayer * BAR_WIDTH
-			g = int(255 * a**3)
-			cv2.rectangle(frame, (x, y), (x+BAR_WIDTH-2, y-BAR_WIDTH-2), (0, g, 0), -1)
+			y = int(a*10)
+			cv2.rectangle(chart, (x, 400), (x + BAR_WIDTH, 400-y), getColour(iLayer), -1)
+			x += BAR_WIDTH + PADDING
+	cv2.imshow("bar chart", chart)
+	###########################
+
+	###########################
+	### Visualize bar chart ###
+	BAR_WIDTH, PADDING = 10, 2
+	LAYER_WIDTH = 16 * (BAR_WIDTH+PADDING) - PADDING + BAR_WIDTH
+	GRAPH_WIDTH = BAR_WIDTH + NCHANNELS * LAYER_WIDTH
+	
+	chart = np.ones((800, GRAPH_WIDTH, 3), dtype=np.uint8) * 20
+	for iChannel in range(0, 8):
+		pixels = fullActivationsT[0][iChannel*16 : (iChannel+1) * 16]
+		x = BAR_WIDTH + iChannel * LAYER_WIDTH
+		for iPixel, pixel in enumerate(pixels):
+			y = int(pixel * 100)
+			cv2.rectangle(chart, (x, 400), (x + BAR_WIDTH, 400-y), getColour(iChannel), -1)
+			x += BAR_WIDTH + PADDING
+	cv2.imshow("bar chart 2", chart)
+			# print("\t", iPixel, pixel)
+		# 	x = BAR_WIDTH + iChannel * LAYER_WIDTH + iPixel
+		# 	for iA, a in enumerate(activations):
+		# 		y = int(a*10)
+		# 		cv2.rectangle(chart, (x, 400), (x + BAR_WIDTH, 400-y), getColour(iLayer), -1)
+		# 		x += BAR_WIDTH + PADDING
+		# cv2.imshow("bar chart", chart)
+	###########################
+
 
 	cv2.imshow("frame",frame)
+
 	# writer.write(frame)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
